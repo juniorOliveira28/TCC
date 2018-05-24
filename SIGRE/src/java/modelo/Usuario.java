4611acc5652c84e6002e11aa3ef31f257d323a60
Junior Oliveira <oliveirajunior.cne@gmail.com>
2018-05-23 21:28:25 -0300

    Sigre

---
+++ SIGRE/src/java/modelo/Usuario.java
@@ -0,0 +1,36 @@
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+@Entity
+public class Usuario extends Pessoa implements Serializable{
+    
+    private String perfil;
+    private String senha;
+    private Boolean enable = true;
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+}
